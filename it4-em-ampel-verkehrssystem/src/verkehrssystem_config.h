77ffc0c3dde5800455ff171338f0f66ba2d0feea
ptrLx <13c@posteo.de>
2021-12-25 23:34:03 +0100

    verkehrssystem general setup

--- it4-em-ampel-verkehrssystem/src/verkehrssystem_config.h
+++ it4-em-ampel-verkehrssystem/src/verkehrssystem_config.h
@@ -1,11 +1,15 @@
-//* Zustände der Ampel
-#define HAUPTSTRASSE_GRUEN 0x00
-#define NEBENSTRASSE_GRUEN 0x01
+//** Events
+//* Zustände der Ampel (one hot)
+#define HS_GRUEN (1 << 0)  // Hauptstraße hat grün
+#define NS_GRUEN (1 <<)    // Nebenstraße und Fußgänger haben grün
 
 //* Timer events (one hot)
 #define HS_TIMER_TICK (1 << 0)  // Hauptstraße hatte für best. Zeit grün
-#define NS_TIMER_TICK (1 << 1)  // Nebenstraße hatte für best. Zeit grün
+#define NS_TIMER_TICK (1 << 1)  // Nebenstraße und Fußgänger hatten für best. Zeit grün
 
-//* Events
-#define FG_EVENT 0x00  // Fußgänger drückt Knopf
-#define NS_EVENT 0x00  // Auto fährt auf Induktionsplatte
+//* Events (one hot)
+#define FG_EVENT (1 << 0)  // Fußgänger drückt Knopf
+#define NS_EVENT (1 << 1)  // Auto fährt auf Induktionsplatte
+
+//** Timer (one hot)
+#define TRAFFIC_LIGHT (1 << 0)  // Timer for current phase
